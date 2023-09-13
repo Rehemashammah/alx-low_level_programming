5caca5d550afee017356229cdceb670761d822f1
Rehemashammah <rehemashammah@yahoo.com>
2023-09-13 10:50:39 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - Entry point for function definition
+ * @a: Parameter to be used
+ * Return: 0 (success)
+ */
+int _abs(int a)
+{
+
+	if (a < 0)
+	{
+		return (-a);
+	}
+	else
+	{
+		return (a);
+	}
+}
