d7707f107c4d11defeec7e656f643350e3e61008
Rehemashammah <rehemashammah@yahoo.com>
2023-09-18 23:43:22 -0700

    Swipe swipe

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Function to swipe integers
+ * @a: Parameter one passed
+ * @b: Parameter two passed
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
