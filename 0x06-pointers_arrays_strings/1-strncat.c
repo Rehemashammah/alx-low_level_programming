52187ef5215d2a09bd4f09556b77a34346d3921f
Rehemashammah <rehemashammah@yahoo.com>
2023-09-21 12:32:48 -0700

    Concatenate n number of times

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,5 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len = strlen(dest);
-	int i;
-
-	for (i = 0; i < n && *src != '\0'; i++)
-	{
-		dest[len + i] = src[i];
-		src++;
-	}
-	dest[len + i] = '\0';
-	return (dest);
+	return (strncat(dest, src, n));
 }
