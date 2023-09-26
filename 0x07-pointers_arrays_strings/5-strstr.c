00c10288b372165821580f757d917a0899fb3a67
Rehemashammah <rehemashammah@yahoo.com>
2023-09-26 00:47:58 -0700

    Substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - Function to locate a substring
+ * @haystack: para 1 passsed
+ * @needle: para 2 passed
+ * Return: pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
