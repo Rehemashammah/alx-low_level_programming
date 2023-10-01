fcd46652aebd1c0fcb38736bd3e346febf8ba3ac
Rehemashammah <rehemashammah@yahoo.com>
2023-09-30 20:56:03 -0700

    New name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: takes the number of command line arguments passed
+ * @argv: array of size argc
+ * Return: Always 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
