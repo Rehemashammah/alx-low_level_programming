55da2bfd4464f006b0075c4b007c976a50b1d11d
Rehemashammah <rehemashammah@yahoo.com>
2023-10-06 06:18:57 -0700

    String concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,9 +15,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 	unsigned int len1, len2, i;
 
-	if ((s1 == NULL) && (s2 == NULL))
+	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
