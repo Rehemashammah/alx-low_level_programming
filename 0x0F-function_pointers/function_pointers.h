9eb78ecd359928fa0f67531c2405260f5a9e3a49
Rehemashammah <rehemashammah@yahoo.com>
2023-10-13 09:11:43 -0700

    Print name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_P_H
+#define FUNCTION_P_H
+
+int _putchar(char c);
+
+void print_name(char *name, void (*f)(char *));
+
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
