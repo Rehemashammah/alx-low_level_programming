ec786f15c5a1ece0e624d1207cacee63870d9641
Rehemashammah <rehemashammah@yahoo.com>
2023-10-26 00:37:15 +0300

    Get node at nth index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	}
 
-	for (i = 1; i < index; i++)
+	for (i = 0; i < index; i++)
 	{
 		head = head->next;
 		if (head == NULL)
