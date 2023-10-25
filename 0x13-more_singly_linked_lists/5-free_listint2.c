ee0c5b0e3f5da7d002d00bff5a6ea3c4b939fc09
Rehemashammah <rehemashammah@yahoo.com>
2023-10-25 23:06:49 +0300

    Free listss

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,15 +10,14 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current, *next;
+	listint_t *temp;
 
-	current = *head;
-
-	while (current != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
-	*head = NULL;
 }
