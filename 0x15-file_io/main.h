70b940a299fc376a8894e1b2b37d73e403e98d27
Rehemashammah <rehemashammah@yahoo.com>
2023-10-31 05:44:09 +0300

    Copy

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,5 +17,10 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+void error_98(int file_from, char *buffer, char *argv);
+
+void error_98(int file_from, char *buffer, char *argv);
+
+void error_100(int file_from, char *buffer);
 
 #endif
